087e242205592edd3fbff15a0169d819bbb1cf7a
Abby Waddell <wabby.404@gmail.com>
2022-09-06 12:23:12 -0400

    removed fade app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Fade from 'react-reveal/Fade';
 
 function App() {
   return (
